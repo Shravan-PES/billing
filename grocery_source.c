301a6df167f5fbb345ddc8d6783aea83cc9dbcea
Shravan-PES <165535729+Shravan-PES@users.noreply.github.com>
2024-04-21 18:59:01 +0530

    Update grocery_source.c

--- grocery_source.c
+++ grocery_source.c
@@ -1,35 +1,39 @@
 #include "grocery_header.h"
 #include <stdio.h>
+#include <string.h>
 #include <conio.h>
-    // int r=10,c=25;
-    char item_name[10][25]={"Books","Bottle","Pens","Brush","Soap"};
-    float item_cost[10]={60,100,10,30,20};
-    char item_id[10][5]={"111","112","113","114","115"};
-    int stock[10]={20,100,30,10,20};
-    char customer_name[10][20]={"Rajesh","Rahul","Sameer","Ajay","Akshay"};
-    char phone[10][11]={"9486726543","7492546186","8694142390","9446514235","9875562341"};
-    char customer_id[10][5]={"232","233","234","235","236"};
-    int count=5;
-    char new_item_name[10][50];
-    int new_item_quantity[10];
+#include <stdlib.h>
 
-    void item_entry()
+   struct Item {
+    char name[50];
+    float price;
+    int quantity;
+};
+
+struct Customer {
+    char name[50];
+    char phone[11];
+    char id[5];
+};
+struct Item item[10]={{"Books",60,20},{"Bottle",75,50},{"Pens",10,30},{"Brush",30,10},{"Soap",20,20}};
+struct Customer customer[10]={{"Rajesh","9486726543","232"},{"Rahul","7492546186","233"},{"Sameer","8694142390","234"},{"Ajay","9446514235","235"},{"Akshay","9875562341","236"}};
+int count = 5;
+
+void item_entry()
 {  
     printf("----------------ITEM ENTRY------------------\n");
     if(count<=10){
     printf("Enter the name of item you want to add\n ");
-    scanf("%s",&item_name[count]);
+    scanf("%s",&item[count].name);
     printf("Enter the cost of the item\n ");
-    scanf("%f",&item_cost[count]);
-    printf("Enter the id of item\n ");
-    scanf("%s",&item_id[count]);
+    scanf("%f",&item[count].price);
     printf("Enter the number of item available in stock\n");
-    scanf("%d",&stock[count]);
+    scanf("%d",&item[count].quantity);
     printf("Item added successfully\n");
     count++;
     }
     else{
-        printf("Maximum count reached\n");
+        printf("Maximum count reached");
     }
 }
 
@@ -38,11 +42,11 @@ void customer_entry()
      printf("----------------CUSTOMER ENTRY------------------\n");
     if(count<=5){
     printf("Enter the name of customer you want to add\n ");
-    scanf("%s",&customer_name[count]);
-    printf("Enter the phone number of the customer\n ");
-    scanf("%s",&phone[count]);
+    scanf("%s",&customer[count].name);
     printf("Enter the id of customer\n ");
-    scanf("%d",&customer_id[count]);
+    scanf("%d",&customer[count].id);
+    printf("Enter the phone number of the customer\n ");
+    scanf("%s",&customer[count].phone);
     printf("Customer added successfully\n");
     }
     else{
@@ -52,79 +56,69 @@ void customer_entry()
 
 void billing()
 {
+    struct purchase 
+    {
+        char nam[50];
+        int quant;
+    } New[10];
     char name[20];
-    // float C[10];
-    int Q[10];
     printf("Enter your name- ");
     scanf("%s",&name);
-    int t=0;
+    int o = 0;
     char c;
-    float total = 0;
-    int item_num;
-    int N[10];
     int quantity;
+    int t = 0;
+    int item_num;
     do 
     {
-        printf("Items available are\n");
-        printf("NAME---PRICE---IN STOCK\n");
-        for(int i=0;i<count;i++)
-        {
-            printf("%d)%s---%.2f---%d\n",i+1,item_name[i],item_cost[i],stock[i]);
-        }
-        
-        printf("Enter c to continue and q to quit\n");
-        c=getch();
-            
-        if(c == 'q') break;
+    printf("Items available are\n");
+    printf("NAME---PRICE---IN STOCK\n");
+    for(int i = 0; i < count; i++) {
+        printf("%d)%s---%.2f---%d\n", i + 1, item[i].name, item[i].price, item[i].quantity);
+    }
+
+    printf("Enter c to Continue and q to Quit\n");
+    c = getch();
+   
+     if(c == 'q') break;
         if(c == 'c') 
         {
-            t++;
-            printf("Enter the item number you want to purchase: ");
-            scanf("%d", &item_num);
-            if(item_num < 1 || item_num > count)
-            {
-                printf("Invalid item number.\n");
-                continue;
-            }
-            printf("Enter the quantity: ");
-            scanf("%d", &quantity);
-            
-            if(quantity <= stock[item_num - 1])
-            {
-                float cost = item_cost[item_num - 1] * quantity;
-                for(int u=t;u<=t;u++)
-                {
-                    N[u-1]=item_num-1;
-                    Q[u-1]=quantity;
-                }
-                stock[item_num - 1] -= quantity;
-                printf("Purchase successful.\n");
-                total += cost;
-            }
-            else 
-            {
-                printf("Item out of stock.\n");
-            }
-        }
-    } while (c != 'q');
+        printf("Enter the item number you want to purchase: ");
+        scanf("%d", &item_num);
 
-    printf("Do you want to print the bill (y/n)");
-    char C=getch();
-    if(C=='y')
-    {
+        if (item_num < 1 || item_num > count) {
+            printf("Invalid item number.\n");
+            continue;
+        }
+        printf("Enter the quantity: ");
+        scanf("%d", &quantity);
+        strcpy(New[o].nam, item[item_num - 1].name); 
+        New[o].quant = quantity; 
+        o++;
+        if (quantity <= item[item_num - 1].quantity) {
+            item[item_num - 1].quantity -= quantity; 
+            printf("Purchase successful.\n");
+            t++;
+        } else {
+            printf("Item out of stock.\n");
+        }
+    }
+    }while (c != 'q');
+
+    printf("Do you want to print the bill (y/n)\n");
+    char C = getch();
+    if (C == 'y') {
         printf("-------Grocery Shop--------\n");
-        printf("Name- %s\n", name);
         printf("ITEM NAME\tQUANTITY\tRATE\t\tTOTAL\n");
-        float gst=0.08;
-        float grand=0;
-        for(int i = 0; i < t; i++) 
-        {
-            float itemTotal = item_cost[N[i]] * Q[i]; // Calculate total cost for each item
-            printf("%s\t\t%d\t\t%.2f\t\t%.2f\n", item_name[N[i]], Q[i], item_cost[N[i]], itemTotal);
-            grand+=itemTotal;
-        }     
-        printf("\nTOTAL=\t%.2f\n",grand);
-        printf("GST(8 percent)=\t%.2f\n",gst*grand);
-        printf("GRAND TOTAL=\t%.2f\n",grand+(gst*grand));
+        float gst = 0.08;
+        float grand = 0;
+        for (int i = 0; i < t; i++) {
+            float itemTotal = item[item_num - 1].price * New[i].quant; // Calculate total cost for each item
+            printf("%s\t\t%d\t\t%.2f\t\t%.2f\n", New[i].nam, New[i].quant, item[item_num - 1].price, itemTotal);
+            grand += itemTotal;
+        }
+        printf("\nTOTAL=\t%.2f\n", grand);
+        printf("GST(8%%)=\t%.2f\n", gst * grand);
+        printf("GRAND TOTAL=\t%.2f\n", grand + (gst * grand));
     }
 }
