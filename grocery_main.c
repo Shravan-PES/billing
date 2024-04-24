9e8ed5c9dacba71f49f8f321ba079685a22f5d8c
Shravan-PES <165535729+Shravan-PES@users.noreply.github.com>
2024-04-24 19:00:04 +0530

    Update grocery_main.c

--- grocery_main.c
+++ grocery_main.c
@@ -1,14 +1,42 @@
-#include "grocery_header.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "grocery_header.h"
+struct Item {
+    char name[50];
+    float price;
+    int quantity;
+};
+
+struct Customer {
+    char name[50];
+    char phone[11];
+    char id[5];
+};
+
+struct Item *item;
+struct Customer *customer;
 
 int main()
 {
+    item = (struct Item *)malloc(10 * sizeof(struct Item));
+    customer = (struct Customer *)malloc(10 * sizeof(struct Customer));
+
+    item[0] = (struct Item){"Books", 60, 20};
+    item[1] = (struct Item){"Bottle", 75, 50};
+    item[2] = (struct Item){"Pens", 10, 30};
+    item[3] = (struct Item){"Brush", 30, 10};
+    item[4] = (struct Item){"Soap", 20, 20};
+
+customer[0] = (struct Customer){"Rajesh", "9486726543", "232"};
+    customer[1] = (struct Customer){"Rahul", "7492546186", "233"};
+    customer[2] = (struct Customer){"Sameer", "8694142390", "234"};
+    customer[3] = (struct Customer){"Ajay", "9446514235", "235"};
+    customer[4] = (struct Customer){"Akshay", "9875562341", "236"};
     int c;
 
     while(c!=5)
     {
-        printf("What do you like to do:\n 1.Item entry\n 2.Costumer entry\n 3.Edit Item\n 4.Compute and Print Bill\n 5.Quit\n\n");
+        printf("What do you like to do :\n 1.Item entry\n 2.Costumer entry\n 3.Edit Item\n 4.Compute and Print Bill\n 5.Quit\n\n");
         printf("Enter your option- ");
         scanf("%d",&c);
         switch(c)
@@ -45,5 +73,7 @@ int main()
             }
         }
     }
+     free(item);
+    free(customer);
     return 0;
 }
