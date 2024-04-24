f81b606d7f82b4cb2e975ad60b79e18f29d45ad2
Shravan-PES <165535729+Shravan-PES@users.noreply.github.com>
2024-04-24 18:55:09 +0530

    Update grocery_make.mk

--- grocery_make.mk
+++ grocery_make.mk
@@ -1,5 +1,5 @@
-a.exe: grocery_source.o grocery_main.o
-	gcc grocery_source.o grocery_main.o
+a.exe: grocery_main.o grocery_source.o
+	gcc grocery_main.o grocery_source.o
 grocery_source.o: grocery_source.c grocery_header.h
 	gcc -c grocery_source.c
 grocery_main.o: grocery_main.c grocery_header.h 
